8f9fe29c5c45f3206ad85683fd31c82d362b04eb
tanugupta02 <tanu.gupta02@nagarro.com>
2023-11-25 01:21:51 +0530

    first commith

--- src/test/java/utils/AnnotationTransformer.java
+++ src/test/java/utils/AnnotationTransformer.java
@@ -1,9 +1,10 @@
 package utils;
 
+import java.lang.reflect.Constructor;
+
 import org.apache.hc.core5.http.Method;
 import org.testng.IAnnotationTransformer;
 import org.testng.annotations.ITestAnnotation;
-import org.yaml.snakeyaml.constructor.Constructor;
 
 public class AnnotationTransformer implements IAnnotationTransformer {
     public void transform(ITestAnnotation annotation, Class testClass, Constructor testConstructor, Method testMethod) {
